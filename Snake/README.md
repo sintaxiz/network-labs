09e6a9641ddfc2993f0f25270636fc5d3e4db9bb
Nataha <natashaleind@gmail.com>
2021-10-25 13:55:13 +0700

    add game interface

--- Snake/README.md
+++ Snake/README.md
@@ -1,3 +1,3 @@
-#Snake(s)
+# Snake(s)
 add `--module-path $PATH_TO_JAVAFX --add-modules javafx.controls,javafx.fxml,javafx.media`
 in vm options to build with javafx
--- Snake/src/main/java/ru/nsu/ccfit/kokunina/controllers/GameController.java
+++ Snake/src/main/java/ru/nsu/ccfit/kokunina/controllers/GameController.java
@@ -1,17 +1,38 @@
 package ru.nsu.ccfit.kokunina.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.TilePane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import ru.nsu.ccfit.kokunina.game.Game;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GameController implements Initializable {
 
+    public ListView<Text> playerList;
+    public Text masterName;
+    public Text fieldSize;
+    public Text foodCount;
+    public TilePane gameField;
     private Game game;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         game = new Game();
+    }
+
+    public void handleExitGameButton(ActionEvent actionEvent) throws IOException {
+        // go to main screen
+        Parent mainMenu = FXMLLoader.load(getClass().getClassLoader().getResource("main_menu.fxml"));
+        Stage currentStage = (Stage) gameField.getScene().getWindow();
+        currentStage.setScene(new Scene(mainMenu));
     }
 }
