e8dff5c99bbaa1f16e501c940b36890167f583a7
Nataha <natashaleind@gmail.com>
2021-10-25 19:02:20 +0700

    add game field and partly connect view and model

--- Snake/src/main/java/ru/nsu/ccfit/kokunina/controllers/GameController.java
+++ Snake/src/main/java/ru/nsu/ccfit/kokunina/controllers/GameController.java
@@ -6,9 +6,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import ru.nsu.ccfit.kokunina.game.Game;
 
 import java.io.IOException;
@@ -22,11 +28,29 @@ public class GameController implements Initializable {
     public Text fieldSize;
     public Text foodCount;
     public TilePane gameField;
+
     private Game game;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         game = new Game();
+        // connect model & view
+        masterName.textProperty().bind(game.masterNameProperty());
+        foodCount.textProperty().bind(game.foodCountProperty());
+        Pair<Integer, Integer> gridSize = game.getGameFieldSize();
+        int rows = gridSize.getKey();
+        int columns = gridSize.getValue();
+        fieldSize.setText(rows + "x" + columns);
+        gameField.setMinSize(columns * 11, rows * 10);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                gameField.getChildren().add(new Rectangle(10,10));
+            }
+        }
+        // bind
+        // add listener
+
+
     }
 
     public void handleExitGameButton(ActionEvent actionEvent) throws IOException {
--- Snake/src/main/java/ru/nsu/ccfit/kokunina/game/Game.java
+++ Snake/src/main/java/ru/nsu/ccfit/kokunina/game/Game.java
@@ -1,12 +1,21 @@
 package ru.nsu.ccfit.kokunina.game;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.util.Pair;
 import ru.nsu.ccfit.kokunina.multicast.MulticastSender;
 
 import java.net.UnknownHostException;
 
 public class Game {
-    public Game() {
+    private final StringProperty foodCount;
+    private final StringProperty masterName;
 
+    public Game() {
+        foodCount = new SimpleStringProperty("300");
+        masterName = new SimpleStringProperty("Danil");
         try {
             Thread sender = new Thread(new MulticastSender());
             sender.start();
@@ -14,5 +23,17 @@ public class Game {
             System.out.println("Can not start game");
             e.printStackTrace();
         }
+    }
+
+    public Pair<Integer, Integer> getGameFieldSize() {
+        return new Pair<>(45, 70);
+    }
+
+    public StringProperty masterNameProperty() {
+        return masterName;
+    }
+
+    public StringProperty foodCountProperty() {
+        return foodCount;
     }
 }
